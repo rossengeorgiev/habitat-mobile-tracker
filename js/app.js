16f7e24124e3b4144f60722e74d115070927acf6
Rossen Georgiev <zoorty@gmail.com>
2013-01-01 04:22:13 +0200

    updated vehicle list, scrollable+expandable
    - added iscroll for inline scrolling
        Works both in portrait and landscape view
    - adjusted main style sheet, so header shadow is over map, sexy
    - made list items expandable, just needs animated rotation of arrow

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var listScroll;
+
 function hideAddressbar()
 {
     window.scrollTo(0, 1);
@@ -5,7 +7,9 @@ function hideAddressbar()
 
 $(window).ready(function() {
     window.onorientationchange = hideAddressbar();
+    listScroll = new iScroll('main', { hScrollbar: false, snap: true });
 
+    // click first icon for popup with the window size
     $('.home').click(function() {
         var size = "";
         size += $(window).width();
@@ -15,7 +19,21 @@ $(window).ready(function() {
         alert(size);
     });
 
+    // reload page when station icon is clicked
     $('.stations').click(function() {
         window.location.href = window.location.href;
+    });
+
+    // expand list items
+    $('.row .header').click(function() {
+        var e = $(this).parent();
+        if(e.hasClass('active')) {
+            e.removeClass('active');
+            e.find('.data').slideUp();
+        } else {
+            e.addClass('active');
+            e.find('.data').slideDown();
+        }
+        listScroll.refresh();
     });
 });
