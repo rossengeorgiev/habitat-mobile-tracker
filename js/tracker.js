rossengeorgiev/habitat-mobile-tracker

commit 03fde5f1a6f2773b2e5a3db9fdf9f587e0568fb2

refresh graph after new data; yaxes min range is 0
yaxes min range
---------------
This will make the graph readable, when there is
data with a tiny range.

However, if the data at any point goes bellow 0,
the limit is removed and the axis scales automatically.

cache.manifest
@@ -1,5 +1,5 @@
 CACHE MANIFEST
-# version 52
+# version 53
 
 # gogole maps files
 http://maps.google.com/maps/api/js?v=3.10&sensor=false&key=AIzaSyCOqkcNey4CCyG4X0X5qxHAhCgD8g5DwXg

js/tracker.js
@@ -412,6 +412,10 @@ function updateVehicleInfo(index, position) {
 
   $('.portrait .vehicle'+index).html(a + p + b);
   $('.landscape .vehicle'+index).html(a + l + b);
+
+  // update graph is current vehicles is followed
+  if(vehicles[index].follow) updateGraph(index);
+
   return true;
 }
 
@@ -792,11 +796,12 @@ function graphAddLastPosition(idx) {
     var ts = (new Date(new_data.gps_time)).getTime(); // flot needs miliseconds for time
     var series_idx = 1;
 
+    // altitude is always the first series
     if(data[0] === undefined) {
         data[0] = {
                     label: "altitude = 0",
                     color: '#33B5E5',
-                    yaxis: 1,
+                    yaxis: series_idx,
                     lines: { show:true, fill: true, fillColor: "rgba(51, 181, 229, 0.1)" },
                     data: []
                   };
@@ -804,11 +809,14 @@ function graphAddLastPosition(idx) {
 
     // push latest altitude
     data[0].data.push([ts, parseInt(new_data.gps_alt)]);
+    if(parseInt(new_data.gps_alt) < 0) delete plot_options.yaxes[series_idx-1].min;
+
+    // the rest of the series is from the data field
     var json = $.parseJSON(new_data.data);
 
     $.each(json, function(k, v) {
-        if(isNaN(v)) return;
-        if(series_idx > 7) return;
+        if(isNaN(v)) return;        // only take data that is numerical
+        if(series_idx > 7) return;  // up to 7 seperate data plots
 
         var i = series_idx++;
 
@@ -821,7 +829,8 @@ function graphAddLastPosition(idx) {
 
            if(isInt(v)) $.extend(true, data[i], { noInterpolate: true, lines: { steps: true }});
         }
-        data[i].data.push([ts, parseFloat(v)]);
+        data[i].data.push([ts, v]);
+        if(v < 0) delete plot_options.yaxes[i+1].min;
     });
 }
 

js/plot_config.js
@@ -21,18 +21,15 @@ var plot_options = {
         mode: "x"
     },
     yaxes: [
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
-        {show: false},
+        {show: false, min: 0 },
+        {show: false, min: 0 },
+        {show: false, min: 0 },
+        {show: false, min: 0 },
+        {show: false, min: 0 },
+        {show: false, min: 0 },
+        {show: false, min: 0 },
+        {show: false, min: 0 },
+        {show: false, min: 0 },
     ],
     xaxes: [
         {

commit 697367e0aacb24d67eb6fd5fea7f07a1b2dec7db

fix type casting bug introduced in 03fde5f

cache.manifest
@@ -1,5 +1,5 @@
 CACHE MANIFEST
-# version 53
+# version 54
 
 # gogole maps files
 http://maps.google.com/maps/api/js?v=3.10&sensor=false&key=AIzaSyCOqkcNey4CCyG4X0X5qxHAhCgD8g5DwXg

js/tracker.js
@@ -829,8 +829,8 @@ function graphAddLastPosition(idx) {
 
            if(isInt(v)) $.extend(true, data[i], { noInterpolate: true, lines: { steps: true }});
         }
-        data[i].data.push([ts, v]);
-        if(v < 0) delete plot_options.yaxes[i+1].min;
+        data[i].data.push([ts, parseFloat(v)]);
+        if(parseFloat(v) < 0) delete plot_options.yaxes[i+1].min;
     });
 }
 